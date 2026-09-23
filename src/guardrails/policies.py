8d274b4fc8d50dfcc1dc97766e9faedb3691f6d7
zeynep <49367777+zeinab-khj@users.noreply.github.com>
2026-09-24 02:28:44 +0330

    Create policies.py

---
+++ src/guardrails/policies.py
@@ -0,0 +1,22 @@
+SENSITIVE_TOOLS = {
+    "cancel_order",
+    "initiate_return",
+    "initiate_exchange",
+    "log_payment_dispute",
+    "update_account",
+}
+
+ALLOWED_TOOLS = {
+    "cancel_order",
+    "initiate_exchange",
+    "get_order_status",
+    "initiate_return",
+    "search_products",
+    "log_payment_dispute",
+    "update_account",
+    "log_complaint",
+    "add_to_cart",
+    "update_cart_qty",
+    "escalate_to_agent",
+    "get_recommendations",
+}
